2d3ba82fba5f3b281cf8af2f18b18e2c117e2a8e
student <student@FinAid2019-8.hsd1.ma.comcast.net>
2026-06-07 22:28:34 -0400

    Created first React app

---
+++ react-practice-app/src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  const name = "Fru";
+
+  return (
+    <div>
+      <h1>Welcome, {name}!</h1>
+
+      <h2>About Me</h2>
+
+      <ul>
+        <li>Computer Science Major</li>
+        <li>AI Concentration</li>
+        <li>Resident Assistant</li>
+        <li>NSBE Programs Chair</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
